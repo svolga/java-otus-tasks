53f1881dff74e1bb53f802116e46287d3d463554
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-08-05 18:40:50 +0300

    feat: Выполнить оптимизацию кода при работе gc

---
+++ hw04-gc/src/main/java/ru/calculator/Data.java
@@ -0,0 +1,13 @@
+package ru.calculator;
+
+public class Data {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
