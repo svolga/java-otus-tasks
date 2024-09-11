cf1f947b931595713171c3dbce8699c90e403ccf
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-09-11 23:37:57 +0300

    feat: Написать эмулятор АТМ (банкомата)

--- hw06-solid/src/main/java/ru/otus/service/CalculatorServiceImpl.java
+++ hw06-solid/src/main/java/ru/otus/service/CalculatorServiceImpl.java
@@ -18,15 +18,15 @@ public class CalculatorServiceImpl implements CalculatorService {
     public Map<Banknote, Integer> calculate(int requestSum) {
         List<Banknote> list = walletService.getBanknotes();
         Map<Banknote, Integer> extractedBanknotes = getBanknotesWithRequestedSum(requestSum, list);
-        return extractedBanknotes.isEmpty() ? null : walletService.getBanknotes(extractedBanknotes);
+        return extractedBanknotes == null || extractedBanknotes.isEmpty() ? null : walletService.getBanknotes(extractedBanknotes);
     }
 
     private Map<Banknote, Integer> getBanknotesWithRequestedSum(int sum, List<Banknote> values) {
         Map<Banknote, Integer> map = new HashMap<>();
-        for (int i = 0; i < values.size(); i++) {
-            if (sum >= values.get(i).getValue()) {
-                map.put(values.get(i), map.getOrDefault(values.get(i), 0) + 1);
-                sum -= values.get(i).getValue();
+        for (Banknote value : values) {
+            if (sum >= value.getValue()) {
+                map.put(value, map.getOrDefault(value, 0) + 1);
+                sum -= value.getValue();
             }
         }
         return sum == 0 ? map : null;
