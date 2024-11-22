c0d461d1226481e8e0787af64f9c14e227b7d1c2
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-11-22 15:45:46 +0300

    fix: Исправить переменные

--- hw15-thread/src/main/java/ru/otus/Task.java
+++ hw15-thread/src/main/java/ru/otus/Task.java
@@ -5,11 +5,11 @@ public class Task implements Runnable{
     private final TaskPrinter printer;
     private final int min;
     private final int max;
-    private final boolean isRunFirst;
+    private final boolean isRunningFirst;
 
-    public Task(TaskPrinter printer, boolean isRunFirst, int min, int max) {
+    public Task(TaskPrinter printer, boolean isRunningFirst, int min, int max) {
         this.printer = printer;
-        this.isRunFirst = isRunFirst;
+        this.isRunningFirst = isRunningFirst;
         this.min = min;
         this.max = max;
     }
@@ -24,13 +24,13 @@ public class Task implements Runnable{
 
     private void up() {
         for (int i = min; i <= max; i++) {
-            printer.runWork(i, isRunFirst);
+            printer.runWork(i, isRunningFirst);
         }
     }
 
     private void down() {
         for (int i = max - 1; i > min; i--) {
-            printer.runWork(i, isRunFirst);
+            printer.runWork(i, isRunningFirst);
         }
     }
 
